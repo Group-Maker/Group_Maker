8e063c3d9c41f7e34122f82cc1d8ec5c3c29724e
chaerin-dev <chaerin.dev@gmail.com>
2022-10-17 01:26:35 +0900

    Refact: rename key, function, variable

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,13 +1,13 @@
-const KEY = 'state';
+const KEY = 'organization';
 
 // localStorage에 state를 저장한다.
-export const saveState = newState => {
-  const serialized = JSON.stringify(newState);
+export const saveOrganization = newOrganization => {
+  const serialized = JSON.stringify(newOrganization);
   localStorage.setItem(KEY, serialized);
 };
 
 // localStorage에서 state를 취득한다.
-export const loadState = (initialState = {}) => {
+export const loadOrganization = () => {
   const serialized = localStorage.getItem(KEY);
-  return JSON.parse(serialized) || initialState;
+  return JSON.parse(serialized);
 };
