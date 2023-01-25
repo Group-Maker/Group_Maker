798cfe7b64eb1e3ba02e9f937fec1adee9aec138
Ellis <nardi0302@gmail.com>
2023-01-25 17:52:08 +0900

    feat: Implement life cycle method
    - didMount / didUpdate / willUnmount 메서드를 구현하였습니다
    - 이벤트 핸들러를 등록하고 해제하는 로직을 수정하였습니다
    	- didMount 시점에 등록
    	- willUnmount 시점에 해제
    - 자세한 구현 사항과 한계점은 노션에 기록하였습니다

---
+++ library/CBD/ComponentTree.js
@@ -0,0 +1,78 @@
+/* eslint-disable lines-between-class-members */
+/* eslint-disable import/prefer-default-export */
+
+import { subscribeEvents, unSubscribeEvents } from './eventHandler';
+
+class ComponentNode {
+  constructor(componentInstance, parentId) {
+    this.component = componentInstance;
+    this.parentId = parentId;
+    this.children = [];
+  }
+}
+
+export class ComponentTree {
+  // 컴포넌트를 담아두는 객체
+  // 탐색 없이 모든
+  #components = {};
+  // 컴포넌트 호출 순서를 관리하는 배열
+  // 컴포넌트를 트리에 추가할 때 부모 컴포넌트를 확인할 수 있다
+  #callStack = [];
+  get callStack() {
+    return this.#callStack;
+  }
+  get components() {
+    return this.#components;
+  }
+  pushToCallStack(componentId) {
+    this.#callStack.push(componentId);
+  }
+
+  popCallStack() {
+    this.#callStack.pop();
+  }
+
+  add(component) {
+    const componentId = component.id;
+    const parentId = this.#callStack.at(-1) || null;
+
+    this.#components[componentId] = new ComponentNode(component, parentId);
+    parentId && this.#components[parentId].children.push(componentId);
+  }
+
+  remove(componentId) {
+    const { component, parentId, children } = this.#components[componentId];
+
+    component.willUnmount?.();
+    component.eventHandlers && unSubscribeEvents(component.eventHandlers);
+
+    delete this.#components[componentId];
+    if (this.#components[parentId]) {
+      this.#components[parentId].children = this.#components[parentId].children.filter(id => id !== componentId);
+    }
+
+    children.forEach(childId => this.remove(childId));
+  }
+
+  update(component) {
+    const { isMounted } = component;
+    if (isMounted) {
+      component.didUpdate?.();
+    } else {
+      component.didMount?.();
+      component.eventHandlers && subscribeEvents(component.eventHandlers);
+      component.isMounted = true;
+    }
+  }
+
+  trav(componentId, callback) {
+    if (!componentId || !this.#components[componentId]) {
+      return;
+    }
+    console.log(`travling`, componentId);
+    const { component, children } = this.#components[componentId];
+
+    callback(component, componentId);
+    children.forEach(childId => this.trav(childId, callback));
+  }
+}
--- library/CBD/index.js
+++ library/CBD/index.js
@@ -1,5 +1,5 @@
-import render from './render.js';
-import Component from './component.js';
+import { render } from './render.js';
+import Component from './Component.js';
 import useGlobalState from './state.js';
 
 export { render, Component, useGlobalState };
