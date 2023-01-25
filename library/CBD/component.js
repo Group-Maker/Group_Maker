d58a14269cf65e3a3cced9733d0d08398796ae4a
Ellis <nardi0302@gmail.com>
2023-01-25 17:52:08 +0900

    feat: Implement life cycle method
    - didMount / didUpdate / willUnmount 메서드를 구현하였습니다
    - 자세한 구현 사항과 한계점은 노션에 기록하였습니다

--- library/CBD/component.js
+++ library/CBD/component.js
@@ -1,23 +1,31 @@
-import { holdEventHandlers } from './eventHandler.js';
-import render from './render.js';
+import { render, componentTree } from './render.js';
+import { validateEventHandlerInfo } from './eventHandler.js';
 
 class Component {
   constructor(props) {
     this.props = props;
     this.state = null;
-    this.componentId = this.constructor.name + '-' + self.crypto.randomUUID().slice(0, 8);
 
-    this.updateEventHandlers();
+    this.id = this.constructor.name + '-' + self.crypto.randomUUID().slice(0, 8);
+    this.isMounted = false;
+    this.eventHandlers = null;
+
+    this.bindEventHandlers();
+    componentTree.add(this);
   }
 
   setState(nextState) {
     this.state = typeof nextState === 'function' ? nextState(this.state) : nextState;
 
-    render(document.querySelector(`[data-component-id="${this.componentId}"]`), this);
+    render(document.querySelector(`[data-component-id="${this.id}"]`), this);
   }
 
   render() {
-    return this.labelDOMStr(this.DOMStr().trim());
+    componentTree.pushToCallStack(this.id);
+    // console.log(componentTree.callStack);
+    const labledDOMStr = this.labelDOMStr(this.DOMStr().trim());
+    componentTree.popCallStack();
+    return labledDOMStr;
   }
 
   DOMStr() {
@@ -30,19 +38,16 @@ class Component {
       throw new Error('컴포넌트는 무조건 하나의 요소로 감싸야 합니다');
     }
 
-    return DOMStr.replace(/<[^>]*>/, openTag => `${openTag.slice(0, -1)} data-component-id="${this.componentId}"/>`);
+    return DOMStr.replace(/<[^>]*>/, openTag => `${openTag.slice(0, -1)} data-component-id="${this.id}"/>`);
   }
 
-  updateEventHandlers() {
-    if (!this.setEvent) {
-      return;
-    }
-    const labeledHandlerInfo = this.setEvent().map(handlerInfo => ({
-      id: this.componentId,
-      ...handlerInfo,
-    }));
+  bindEventHandlers() {
+    if (this.setEvent) {
+      const eventHandlers = this.setEvent();
+      validateEventHandlerInfo(eventHandlers);
 
-    holdEventHandlers(labeledHandlerInfo);
+      this.eventHandlers = eventHandlers;
+    }
   }
 }
 
