43069eb26cbdb139530a41faceb50c0c534dabd0
Ellis <nardi0302@gmail.com>
2023-03-16 15:13:57 +0900

    feat: implement formating util function
    - x,y,z 값으로 translate3D CSS값을 반환하는 유틸함수를 구현하였습니다

--- src/utils/style.js
+++ src/utils/style.js
@@ -2,6 +2,8 @@
 
 export const appendStyles = (element, style = {}) => {
   Object.entries(style).forEach(([property, value]) => {
-    element.style[property] = value;
+    element.style[property] = typeof value === 'number' ? value + 'px' : value;
   });
 };
+
+export const formatTranslate3DCSS = (x = 0, y = 0, z = 0) => `translate3D(${x}px, ${y}px, ${z}px)`;
