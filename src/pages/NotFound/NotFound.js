cfdd0e658f6b0ae499cb55bb81abedcb4c2d050c
chaerin-dev <chaerin.dev@gmail.com>
2023-03-17 17:06:08 +0900

    Feat: WHOLE NEW STYLE

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -6,7 +6,6 @@ export default class NotFound extends Component {
   DOMStr() {
     return `
       <div class="${style.container}">
-        <h1 class="logoLink">${new Link({ path: '/', content: 'GROUP MAKER' }).render()}</h1>
         <span class="${style.notFoundMsg}">Sorry, the page you are looking for does not exist.</span>
         <div class="${style.goToHome}">${new Link({ path: '/', content: 'GO TO HOME' }).render()}</div>
       </div>`;
